51c515b4f1e19597066696520f835e25551e2651
vvp45 <vikas.v.patil1696@gmail.com>
2018-08-17 17:26:34 +0530

    Upload and Download file

---
+++ upload/config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :resumes, only: [:index, :new, :create, :destroy]   
+  root "resumes#index"  
+
+  get 'resumes/index'
+
+  get 'resumes/new'
+
+  get 'resumes/create'
+
+  get 'resumes/destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
