8e86a06148f0ced829867e7f45a5659b472511b7
Vikas Patil <30790175+vvp45@users.noreply.github.com>
2019-12-07 19:25:48 +0530

    Exception Demo program added

---
+++ Ruby/Exception.rb
@@ -0,0 +1,8 @@
+begin     
+    puts 'code before raise.'     
+    raise 'exception occurred.'     
+    puts 'code after raise.'     
+rescue     
+    puts 'I am rescued.'     
+end     
+puts 'code after begin block.'    
